e7e1d90fa410128cd7dd97bc6ba4cf99e3d8878b
Gabriel-hlp <gabrieel.h12@gmail.com>
2025-05-27 17:14:00 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,7 @@
+#Solicitar ao usuario o numero desejado
+Numero = int(input("Digite um numero para ver a tabuada:"))
+
+#Gera a tabuada de 1 a 10 com for
+print(f"\nTabuada do {Numero}:")
+for Número in 1,2,3,4,5,6,7,8,9,10:
+    print(f"{Numero} x {Número} = {Numero * Número}") 
